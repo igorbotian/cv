db35a7bd19ea56438e4776bbbf101effb97aacac
Igor Botian <igor.botian@gmail.com>
2018-08-13 12:51:55 +0300

    Relevant as of August 2018

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=`dirname "$0"`
 OUTPUT_DIR=${SCRIPT_DIR}/out
+rm -Rfv ${OUTPUT_DIR}
 mkdir -p ${OUTPUT_DIR}
 pdflatex -jobname=cv_igor_botian -output-directory=${OUTPUT_DIR} -halt-on-error cv
